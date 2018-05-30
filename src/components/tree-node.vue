11c9b6ea049a7ab08868820c129f485278ed2f40
zjsxdyw <zjsxdyw@gmail.com>
2018-05-30 19:04:04 +0800

    Update tree-node.vue

--- src/components/tree-node.vue
+++ src/components/tree-node.vue
@@ -1,1 +1,85 @@
+<template>
+  <div class="tree-node">
+    <div class="tree-node-content" @click.stop="handleClick">
+      <span class="icon-right" 
+        :class="{ 
+          'is-leaf': node.isLeaf,
+          expanded: !node.isLeaf && expanded
+        }"></span>
+      <span class="checkbox"
+        :class="{
+          'is-checked': node.checked,
+          'is-indeterminate': node.indeterminate,
+          'is-disabled': node.disabled
+        }"
+        @click.stop="handleCheckChange"></span>
+      <slot :node="node"></slot>
+    </div>
+    <div class="tree-node-children pl18" v-if="expanded">
+      <v-tree-node
+        v-for="child in node.childNodes"
+        :node="child"
+        :key="child.id"
+        :indent="indent"
+        :operation="operation">
+        <template slot-scope="{node}">
+          <slot :node="node"></slot>
+        </template>
+      </v-tree-node>
+    </div>
+  </div>
+</template>
+<script>
+  import ElCollapseTransition from 'element-ui/src/transitions/collapse-transition';
+  import ElCheckbox from 'element-ui/packages/checkbox';
 
+  export default {
+    name: 'VTreeNode',
+
+    componentName: 'ElTreeNode',
+
+    props: {
+      node: {
+        default() {
+          return {};
+        }
+      },
+      indent: {
+        type: Number
+      },
+      operation: {
+        type: Object
+      }
+    },
+
+    components: {
+      ElCollapseTransition,
+      ElCheckbox
+    },
+
+    data() {
+      return {
+        tree: null,
+        expanded: false,
+        childNodeRendered: false,
+        showCheckbox: true,
+        oldChecked: null,
+        oldIndeterminate: null
+      };
+    },
+
+    methods: {
+      handleCheckChange() {
+        this.operation.checkNode(this.node.id, !this.node.checked);
+      },
+      handleClick() {
+        if(this.node.isLeaf) {
+          this.handleCheckChange();
+        } else {
+          this.expanded = !this.expanded;
+        }
+      }
+    },
+
+  };
+</script>
